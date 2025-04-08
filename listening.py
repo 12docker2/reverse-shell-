eafe574a330a76093ce5c10e59d22fe4c3316616
2x2x2x2 <167359148+12docker2@users.noreply.github.com>
2025-04-08 21:37:16 +0200

    Create listening.py

---
+++ listening.py
@@ -0,0 +1,17 @@
+import os
+
+def main():
+  command = "apt install nc"
+  command2 = "apt install netcat"
+
+  try:
+    os.ystem(command)
+  except Exception as e:
+    os.system(command2)
+
+  command3 = "nc -lvnp 4444"
+  try:
+    os.system(command3)
+  except Exception as e:
+    print(f"Error al ponerse en escucha: {e}")
+
